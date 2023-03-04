9a8f8a99418e8ead29525251c3222f61849e03a8
Ryosuke Iwanaga <riywo@opsbr.com>
2023-03-04 16:43:55 +0000

    feat: add default configurations

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-import { typescript } from "projen";
+import { typescript, github, DevEnvironmentDockerImage } from "projen";
 
-export class OpsBRTypeScriptProject extends typescript.TypeScriptProject {}
+export class OpsBRTypeScriptProject extends typescript.TypeScriptProject {
+  constructor(options: typescript.TypeScriptProjectOptions) {
+    super({
+      authorName: "OpsBR Software Technology Inc.",
+      authorUrl: "https://opsbr.com",
+      authorOrganization: true,
+      devContainer: true,
+      prettier: true,
+      githubOptions: {
+        projenCredentials: github.GithubCredentials.fromApp(),
+      },
+      workflowGitIdentity: {
+        name: "opsbr-bot",
+        email: "opsbr-bot@users.noreply.github.com",
+      },
+      ...options,
+    });
+    this.devContainer?.addDockerImage(
+      DevEnvironmentDockerImage.fromImage(
+        "mcr.microsoft.com/devcontainers/typescript-node:0-18-bullseye"
+      )
+    );
+  }
+}
